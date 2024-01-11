96c9b0980aa17bde58806bb26eb3b1c22225faaa
Guido.Pierri <guido.pierri@sj.se>
2024-01-11 15:51:41 +0100

    deployment error fix

--- pentry/app/login/page.tsx
+++ pentry/app/login/page.tsx
@@ -16,10 +16,10 @@ export default async function LoginPage() {
             <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
                 <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
                     <div className={`${croissant.className} w-32 text-white md:w-36`}>
-                        {/*<AcmeLogo />*/}Login
+                        Login
                     </div>
                 </div>
-                Don't have an account? <Link href="/signup">Sign up</Link>
+                Don&apos;t have an account? <Link href="/signup">Sign up</Link>
                 <LoginForm/>
                 Sign in with Google
                 <SignIn provider={'google'}>Sign in with Google</SignIn>
