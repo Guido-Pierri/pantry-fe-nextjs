98b68ac279fa865790d56ea5772212021c8491a1
Guido.Pierri <guido.pierri@sj.se>
2024-01-25 23:01:00 +0100

    error fix

--- pentry/app/dashboard/save-items/[gtin]/page.tsx
+++ pentry/app/dashboard/save-items/[gtin]/page.tsx
@@ -6,9 +6,13 @@ export default async function Page({params}: { params: { gtin: string } }) {
     const gtin = params.gtin;
     const item = await fetchItemByGtin(gtin);
     const user = await auth()
-    const userEmail = user?.user?.email as string
-    const userFromDatabase = await fetchUserByEmail(userEmail)
-    const id = userFromDatabase?.id as string
+    const session = await auth()
+    const token = session?.token;
+    const userEmail = session?.user?.email
+    if (!token || !userEmail) return null
+    const userFromDatabase = await fetchUserByEmail(userEmail, token)
+    if (!userFromDatabase) return null
+    const id = userFromDatabase?.id
     const pantry = await fetchPantryByUserId(id)
     const pantryId = pantry?.id
     console.log('pantryId', pantryId)
