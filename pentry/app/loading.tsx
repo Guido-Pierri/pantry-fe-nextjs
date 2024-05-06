3894b98ece49b2a38198f14457d285c3e03e30f3
Guido.Pierri <guido.pierri@sj.se>
2024-05-06 11:43:01 +0200

    added suspense with loading.tsx when fetching search data

--- pentry/app/loading.tsx
+++ pentry/app/loading.tsx
@@ -2,11 +2,10 @@ import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import {log} from "node:util";
 
-export default function Loading({marginTopProp}: { marginTopProp?: string }) {
-    console.log('marginTopProp', marginTopProp)
+export default function Loading() {
     return (
 
-        <CircularProgress sx={{display: 'flex', justifyItems: 'center', alignItems: 'center', marginTop: marginTopProp}}
+        <CircularProgress sx={{display: 'flex', justifyItems: 'center', alignItems: 'center', marginTop: '50%'}}
                           color={'primary'}/>
     );
 }
