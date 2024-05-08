d0b5d9fc8de763116cf20e1ec44b31f4407729bf
Guido.Pierri <guido.pierri@sj.se>
2024-05-08 15:07:50 +0200

    implemented mui

--- pentry/app/ui/signup-form.tsx
+++ pentry/app/ui/signup-form.tsx
@@ -1,13 +1,20 @@
 'use client';
 import {useFormState, useFormStatus} from "react-dom";
 import {registerUser} from "@/app/lib/actions";
-import {useState} from "react";
+import React, {useState} from "react";
 import {InputType} from "node:zlib";
 import {ExclamationCircleIcon, EyeIcon, EyeSlashIcon, MagnifyingGlassIcon} from "@heroicons/react/24/outline";
 import {Button} from "@/app/ui/button";
 import {ArrowRightIcon} from "@heroicons/react/20/solid";
 import {auth} from "@/auth";
 import {useSession} from "next-auth/react";
+import {Box, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField} from "@mui/material";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+import Fab from "@mui/material/Fab";
+import theme from "@/theme";
+import {FormControl} from "@mui/base";
+import {Visibility, VisibilityOff} from "@mui/icons-material";
 
 export default function SignUpForm() {
 
@@ -17,36 +24,82 @@ export default function SignUpForm() {
     const toggleVisblePass = () => {
         setIsVisiblePass((prev) => !prev)
     };
+    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
     return (
-        <form action={dispatch} className={'flex flex-col'}>
-            <label htmlFor={"firstName"}>First name</label>
-            <input id={"firstName"} name={"firstName"} type={"text"} required={true}/>
-            <label htmlFor={"lastName"}>Last name</label>
-            <input id={"lastName"} name={"lastName"} type={"text"} required={true}/>
-            <label htmlFor={"email"}>Email</label>
-            <input id={"email"} name={"email"} type={"email"} required={true}/>
-            <label htmlFor={"password"}>Password</label>
-            <div className="relative flex flex-1">
-                <input id={"password"} name={"password"} type={isVisiblePass ? "text" : "password"}
-                       required={true}></input>
-                {isVisiblePass ? (<EyeSlashIcon
-                    className="absolute right-3/4 top-1/2 h-[18px] w-[18px] -translate-y-1/2 cursor-pointer"
-                    //className="w-4 cursor-pointer"
-                    onClick={toggleVisblePass}/>) : <EyeIcon onClick={toggleVisblePass}
-                                                             className="absolute right-3/4 top-1/2 h-[18px] w-[18px] -translate-y-1/2 cursor-pointer"
-                />}
-            </div>
-            <label htmlFor={"confirmPassword"}>Confirm password</label>
-            <div className="relative flex flex-1">
+        <Box m={1}>
 
-                <input id={"confirmPassword"} name={"confirmPassword"} type={isVisiblePass ? "text" : "password"}
-                       required={true}></input>
-                {isVisiblePass ? (<EyeSlashIcon
-                    className="absolute right-3/4 top-1/2 h-[18px] w-[18px] -translate-y-1/2 cursor-pointer"
-                    //className="w-4 cursor-pointer"
-                    onClick={toggleVisblePass}/>) : <EyeIcon onClick={toggleVisblePass}
-                                                             className="absolute right-3/4 top-1/2 h-[18px] w-[18px] -translate-y-1/2 cursor-pointer"/>}
-            </div>
+            <form action={dispatch} className={'flex flex-col'}>
+                <div>
+                    <TextField id="firstName"
+                               label="First name"
+                               variant="outlined"
+                               required={true}
+                               fullWidth={true}
+                    />
+                </div>
+                <div>
+                    <TextField id="lastName"
+                               label="Last name"
+                               variant="outlined"
+                               required={true}
+                               fullWidth={true}
+                               sx={{marginTop: '1rem'}}/>
+                </div>
+                <div>
+                    <TextField id="email"
+                               label="Email"
+                               variant="outlined"
+                               required={true}
+                               fullWidth={true}
+                               sx={{marginTop: '1rem'}}/>
+
+                </div>
+                <FormControl>
+                    <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={isVisiblePass ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={toggleVisblePass}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {isVisiblePass ? <VisibilityOff/> : <Visibility/>}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Password"
+                        fullWidth={true}
+                    />
+                </FormControl>
+                <FormControl>
+                    <InputLabel htmlFor="confirm-password">Confirm Password</InputLabel>
+                    <OutlinedInput
+                        id="confirm-password"
+                        type={isVisiblePass ? 'text' : 'password'}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={toggleVisblePass}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {isVisiblePass ? <VisibilityOff/> : <Visibility/>}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Confirm password"
+                        fullWidth={true}
+                    />
+                </FormControl>
+
+            </form>
             <SignupButton/>
             <div
                 className="flex h-8 items-end space-x-1"
@@ -60,7 +113,7 @@ export default function SignUpForm() {
                     </>
                 )}
             </div>
-        </form>
+        </Box>
     );
 }
 
@@ -68,8 +121,9 @@ function SignupButton() {
     const {pending} = useFormStatus();
 
     return (
-        <Button className="mt-4 w-full" aria-disabled={pending}>
+        <Fab variant={'extended'} color={'primary'} sx={{marginTop: '1rem'}}
+             aria-disabled={pending}>
             Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50"/>
-        </Button>
+        </Fab>
     );
 }
