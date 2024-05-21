a5a934ec7e9b064e2a8b7cdd77018a6fc446f172
Guido.Pierri <guido.pierri@sj.se>
2024-05-21 08:37:19 +0200

    break down recipes to a ui component

--- pentry/app/dashboard/recipes/page.tsx
+++ pentry/app/dashboard/recipes/page.tsx
@@ -9,6 +9,7 @@ import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
 import CardActionArea from "@mui/material/CardActionArea";
 import Link from "@mui/material/Link";
+import Recipes from "@/app/ui/recipes/recipes";
 
 export default async function Page() {
 
@@ -16,10 +17,6 @@ export default async function Page() {
     console.log('ingredientsArray', ingredientsArray)
     const ingredientsFiltered = (ingredientsArray).filter((item) => !item.includes(' '))
     console.log('ingredientsFiltered', ingredientsFiltered)
-    const ingredientString = ingredientsArray?.map((item) => item).join('');
-    console.log('ingredientString', ingredientString)
-    const nrOfElements = (ingredientsArray).length + 1;
-    const randomIndex = Math.floor(Math.random() * nrOfElements);
     let randomIngredients: string[] = [];
 
     if (ingredientsFiltered && ingredientsFiltered?.length > 3) {
@@ -36,42 +33,9 @@ export default async function Page() {
     }
     console.log('randomIngredients', randomIngredients)
     const recipes = await fetchMyKitchenRecipes(randomIngredients);
-    if (!recipes) {
-        return <Box>No recipes found</Box>
-    }
-    console.log('recipes', (recipes)?.data?.recipes)
+
+    console.log('recipes', recipes?.data?.recipes)
     return (
-        <Grid container spacing={2} columns={2} rowSpacing={1}>
-            <Grid item xs={12} md={6}>
-                <Typography sx={{mt: 0, mb: 2}} variant="h6" component="div">
-                    Recipes based on your pantry items
-                </Typography>
-                <List dense={true}>
-                    {recipes?.data?.recipes?.map((item: MyKitchenRecipesApiRecipe) => (
-                        item && item?.image ?
-                            <Link key={item.id} href={item.url}><Card
-                                sx={{marginBottom: '1rem', textDecoration: 'none',}}>
-                                <CardActionArea>
-                                    <CardMedia
-                                        component="img"
-                                        height="140"
-                                        image={item.image}
-                                        alt={item.title}
-                                    />
-                                    <CardContent>
-                                        <Typography gutterBottom variant="h5" component="div">
-                                            {item.title}
-                                        </Typography>
-                                        <Typography variant="body2" color="text.secondary">
-                                            {item.description}
-                                        </Typography>
-                                    </CardContent>
-                                </CardActionArea>
-                            </Card></Link>
-                            : null
-                    ))}
-                </List>
-            </Grid>
-        </Grid>
+        <Recipes recipes={recipes?.data?.recipes}/>
     )
 }
