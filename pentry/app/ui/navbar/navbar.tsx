495d72a22ce71e396098a93e321f3ed28c7ce2a8
Guido.Pierri <guido.pierri@sj.se>
2024-05-24 11:42:52 +0200

    create navbar rendered when user is logged out

--- pentry/app/ui/navbar/navbar.tsx
+++ pentry/app/ui/navbar/navbar.tsx
@@ -1,7 +1,5 @@
 'use client';
 import React from "react";
-import useMediaQuery from "@mui/material/useMediaQuery";
-import Box from "@mui/material/Box";
 import {Session} from "next-auth";
 import NavbarLoggedIn from "@/app/ui/navbar/navbar-logged-in";
 import NavbarLoggedOut from "@/app/ui/navbar/navbar-logged-out";
@@ -10,14 +8,6 @@ import NavbarLoggedOut from "@/app/ui/navbar/navbar-logged-out";
 
 // Exporting Default Navbar to the App.js File
 export default function Navbar({session}: { session: Session }) {
-    if (!session) {
-        return (
-            <Box>Log in</Box>
-        )
-    }
-    const small = useMediaQuery("(max-width:600px)");
-    const full = useMediaQuery("(min-width:600px)");
-
     return (
         session ? <NavbarLoggedIn/> : <NavbarLoggedOut/>
 
