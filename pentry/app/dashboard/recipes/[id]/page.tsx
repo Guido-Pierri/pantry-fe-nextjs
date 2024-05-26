1b73eea3f5e7d82c4fd785a4793e39397a9c74b0
Guido.Pierri <guido.pierri@sj.se>
2024-05-26 23:09:45 +0200

    recipe as card

--- pentry/app/dashboard/recipes/[id]/page.tsx
+++ pentry/app/dashboard/recipes/[id]/page.tsx
@@ -1,5 +1,4 @@
-import Box from "@mui/material/Box";
-import {Typography} from "@mui/material";
+import {Card, CardContent, CardMedia, ListItem, ListItemText, Typography} from "@mui/material";
 import {auth} from "@/auth";
 import {fetchRecipeById} from "@/app/lib/data";
 
@@ -10,12 +9,30 @@ export default async function Page({params}: { params: { id: string } }) {
     }
     const recipe = await fetchRecipeById(params.id);
     if (!recipe) return null
+    console.log('recipe', recipe)
+    console.log('analyzedInstructions', recipe.analyzedInstructions)
     return (
-        <Box>
-            <Typography variant={'h1'}>{recipe.title}</Typography>
-            {/*<Typography variant={'h2'}>{recipe?.analyzedInstructions.map((instruction) => <List><ListItem
-                key={instruction.name}><ListItemText>{instruction.name}</ListItemText></ListItem></List>)}</Typography>
-*/}
-        </Box>
+        <Card>
+            <CardMedia>
+
+                {recipe?.image && <img src={recipe.image} alt={recipe.title}/>}
+            </CardMedia>
+            <CardContent>
+                <Typography variant={'h5'}>{recipe.title}</Typography>
+                <Typography variant={'h6'}>Ingredients</Typography>
+                {recipe?.extendedIngredients.map((ingredient) => (
+                    <ListItem key={ingredient.id}>
+                        <ListItemText>{ingredient.amount} {' '}{ingredient.unit}{' '}{ingredient.name}</ListItemText>
+                    </ListItem>
+                ))}
+                <Typography variant={'h5'}>Instructions</Typography>
+                {recipe?.analyzedInstructions[0]?.steps.map((step, index) => (
+                    <ListItem key={index}>
+                        <ListItemText>{step.step}</ListItemText>
+                    </ListItem>
+                ))}
+            </CardContent>
+
+        </Card>
     )
 }
