31c162e82fa31ea92cf463c3adbe4894ebdbc2d9
Guido.Pierri <guido.pierri@sj.se>
2024-05-27 10:39:42 +0200

    recipe image width and recipe ingredients images set

--- pentry/next.config.js
+++ pentry/next.config.js
@@ -34,6 +34,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/**'
             },
+            {
+                protocol: 'https',
+                hostname: 'img.spoonacular.com',
+                port: '',
+                pathname: '/recipes/**',
+            },
         ],
     },
 }
