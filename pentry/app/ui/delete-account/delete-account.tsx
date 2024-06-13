c8aee67d9ddedcd3062487000d36c9610ec37bb7
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2024-06-13 12:20:57 +0200

    delete user redirects to login page signup page refactor

--- pentry/app/ui/delete-account/delete-account.tsx
+++ pentry/app/ui/delete-account/delete-account.tsx
@@ -1,31 +1,15 @@
 import {User} from "@/app/lib/definitions";
-import {Box, Button, Typography} from "@mui/material";
-import Fab from "@mui/material/Fab";
+import {Box} from "@mui/material";
 import React from "react";
-import {deleteUserFromProfile} from "@/app/lib/actions";
-import DeleteIcon from "@mui/icons-material/Delete";
+
+import DeleteUser from "@/app/ui/delete-account/delete-user";
 
 export default async function DeleteAccount({user}: { user: User }) {
 
     return (
         <Box>
-            <Box display={'flex'} justifyContent={'end'} flexDirection={'column'}>
-                <Typography>We are sorry to see you leave</Typography>
-                <DeleteUser id={user.id}/>
-            </Box>
+            <DeleteUser id={user.id}/>
         </Box>
     )
 }
 
-function DeleteUser({id}: { id: string }) {
-    const deleteUserWithId = deleteUserFromProfile.bind(null, id);
-    return (
-        <form action={deleteUserWithId} style={{marginTop: '1rem'}}>
-            <Button endIcon={<DeleteIcon/>}
-                    color={'warning'}
-                    type={'submit'}>
-                Delete your account
-            </Button>
-        </form>
-    );
-}
---
+++ pentry/app/ui/delete-account/delete-user.tsx
@@ -0,0 +1,31 @@
+'use client';
+import {deleteUserFromProfile} from "@/app/lib/actions";
+import {useFormStatus} from "react-dom";
+import Loading from "@/app/loading";
+import {Box, Button, Typography} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import React from "react";
+
+export default function DeleteUser({id}: { id: string }) {
+    const deleteUserWithId = deleteUserFromProfile.bind(null, id);
+    const {pending} = useFormStatus();
+    console.log('pending', pending)
+    if (pending) {
+        return (
+            <Loading/>
+        );
+    }
+    return (
+        <Box display={'flex'} justifyContent={'end'} flexDirection={'column'}>
+            <Typography>We are sorry to see you leave</Typography>
+            <Box component={'form'} action={deleteUserWithId} style={{marginTop: '1rem'}}>
+                <Button endIcon={<DeleteIcon/>}
+                        color={'warning'}
+                        type={'submit'}>
+                    Delete your account
+                </Button>
+            </Box>
+        </Box>
+
+    );
+}
