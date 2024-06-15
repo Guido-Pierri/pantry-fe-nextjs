1e649c92ca445b884213f5de6217064b74f72686
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2024-06-15 20:05:48 +0200

    refactor pantry as grid

--- pentry/app/ui/pantry/RenderPantry.tsx
+++ pentry/app/ui/pantry/RenderPantry.tsx
@@ -14,58 +14,19 @@ import image from "@/app/images/404-error.png";
 import Button from "@mui/material/Button";
 
 export default function RenderPantry({pantry, user}: { pantry: PantryDto, user: User }) {
-
-
     return (
-        <Box mt={'2rem'}>
+        <Box>
+            <Typography variant={'h6'} sx={{mt: 0, mb: 2}}>
+                Your pantry
+            </Typography>
             {pantry && pantry.items.length > 0 ?
-                <Grid container spacing={2} alignItems="stretch" justifyContent={'center'}>
-                    {/*
-                    <Box mt={'2rem'}>
-                        <Grid container spacing={2} alignItems="stretch">
-                            {results?.length > 0 ? results?.slice(0, displayPosts).map((result, index) => (
-                                <Grid item xs={6} sm={6} md={6} key={index}>
-                                    <Link href={`/dashboard/pantry/add-item/items/${result.gtin}`} key={result.gtin}
-                                          sx={{height: '100%', display: 'flex', flexGrow: 1}}
-                                          underline={'none'}>
-                                        <ResultCard item={result}></ResultCard>
-                                    </Link>
-                                </Grid>
-                            )) : <Box display={"flex"} flexDirection={'column'} alignItems="center" mt={10}
-                            >
-                                <Typography variant={'h5'}>No results found</Typography>
-                                <Image src={image} alt={'not found'}/></Box>}
-                        </Grid>
-                        {displayPosts < results.length ? (
-                            <Button variant={'contained'} fullWidth={true} onClick={loadMore} sx={{marginTop: '1rem'}}>Load
-                                more</Button>) : null}
-                    </Box>
-                */}
-
+                <Grid container spacing={2} alignItems="stretch" justifyContent={'flex-start'}>
                     {pantry.items.map((item: Item) =>
                         <Grid item xs={6} sm={4} md={2} key={item.id}>
                             <PantryListItemCard item={item} user={user}></PantryListItemCard>
                         </Grid>
-                    )}<Box display={'flex'} justifyContent={'space-evenly'} mt={'1rem'}>
-
-                    <Link href={'/dashboard/pantry/add-item'}>
-                        <Fab
-                            color="primary"
-                            aria-label="add"
-                            variant={'extended'}
-                            size={'small'}
-                        >
-                            <AddIcon/>Add Items
-                        </Fab></Link>
-                    <Link href={'/dashboard/search'}>
-                        <Fab size={"small"} color={'primary'} variant="extended"
-                             sx={{marginLeft: '1rem'}}>
-                            <SearchIcon/>
-                            Search for a product
-                        </Fab></Link>
-                </Box>
+                    )}
                 </Grid>
-
                 : <Box sx={{
                     display: 'flex',
                     flexDirection: 'column',
@@ -83,7 +44,23 @@ export default function RenderPantry({pantry, user}: { pantry: PantryDto, user: 
                     />
                     <RenderPantryButtons/>
                 </Box>}
-
+            <Box display={'flex'} justifyContent={'space-evenly'} mt={'1rem'}>
+                <Link href={'/dashboard/pantry/add-item'}>
+                    <Fab
+                        color="primary"
+                        aria-label="add"
+                        variant={'extended'}
+                        size={'small'}
+                    >
+                        <AddIcon/>Add Items
+                    </Fab></Link>
+                <Link href={'/dashboard/search'}>
+                    <Fab size={"small"} color={'primary'} variant="extended"
+                         sx={{marginLeft: '1rem'}}>
+                        <SearchIcon/>
+                        Search for a product
+                    </Fab></Link>
+            </Box>
         </Box>
     )
 }
