ca5a0e518921ee7b6624bbba878e78a9822c0451
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2024-07-07 20:47:07 +0200

    WIP next-intl

--- pentry/next.config.js
+++ pentry/next.config.js
@@ -1,46 +1,47 @@
 /**
  *  @type {import('next').NextConfig}
  */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'dabas.blob.core.windows.net',
-                port: '',
-                pathname: '/media/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'i.pravatar.cc',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'picsum.photos',
-                port: '',
-                pathname: '/**',
-            },
-            {
-                protocol: 'http',
-                hostname: 'res.cloudinary.com',
-                port: '',
-                pathname: '/**'
-            },
-            {
-                protocol: 'https',
-                hostname: 'img.spoonacular.com',
-                port: '',
-                pathname: '/recipes/**',
-            },
-        ],
-    },
-}
-module.exports = nextConfig
+
+const withNextIntl = require("next-intl/plugin")();
+module.exports = withNextIntl({
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "dabas.blob.core.windows.net",
+        port: "",
+        pathname: "/media/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "http",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.spoonacular.com",
+        port: "",
+        pathname: "/recipes/**",
+      },
+    ],
+  },
+});
