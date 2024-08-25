d349820f35dec63b17f3a880111fc90a24b60120
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2024-08-25 16:12:23 +0200

    add translation to profile page and delete account page

--- pentry/app/ui/delete-account/delete-user.tsx
+++ pentry/app/ui/delete-account/delete-user.tsx
@@ -1,31 +1,32 @@
-'use client';
-import {deleteUserFromProfile} from "@/app/lib/actions";
-import {useFormStatus} from "react-dom";
+"use client";
+import { deleteUserFromProfile } from "@/app/lib/actions";
+import { useFormStatus } from "react-dom";
 import Loading from "@/app/loading";
-import {Box, Button, Typography} from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 import React from "react";
+import { useTranslation } from "react-i18next";
 
-export default function DeleteUser({id}: { id: string }) {
-    const deleteUserWithId = deleteUserFromProfile.bind(null, id);
-    const {pending} = useFormStatus();
-    console.log('pending', pending)
-    if (pending) {
-        return (
-            <Loading/>
-        );
-    }
-    return (
-        <Box display={'flex'} justifyContent={'end'} flexDirection={'column'}>
-            <Typography>We are sorry to see you leave</Typography>
-            <Box component={'form'} action={deleteUserWithId} style={{marginTop: '1rem'}}>
-                <Button endIcon={<DeleteIcon/>}
-                        color={'warning'}
-                        type={'submit'}>
-                    Delete your account
-                </Button>
-            </Box>
-        </Box>
-
-    );
+export default function DeleteUser({ id }: { id: string }) {
+  const deleteUserWithId = deleteUserFromProfile.bind(null, id);
+  const { pending } = useFormStatus();
+  const { t } = useTranslation();
+  console.log("pending", pending);
+  if (pending) {
+    return <Loading />;
+  }
+  return (
+    <Box display={"flex"} justifyContent={"end"} flexDirection={"column"}>
+      <Typography>{t("text_leave")}</Typography>
+      <Box
+        component={"form"}
+        action={deleteUserWithId}
+        style={{ marginTop: "1rem" }}
+      >
+        <Button endIcon={<DeleteIcon />} color={"warning"} type={"submit"}>
+          {t("text_delete_account")}
+        </Button>
+      </Box>
+    </Box>
+  );
 }
--- pentry/app/ui/profile-page/table.tsx
+++ pentry/app/ui/profile-page/table.tsx
@@ -1,155 +1,185 @@
-'use client';
-import {updateUserProfile} from "@/app/lib/actions";
-import {Avatar, Box, Button, IconButton, InputAdornment, InputLabel, OutlinedInput, TextField} from "@mui/material";
-import {AccountCircle, Email, Visibility, VisibilityOff} from "@mui/icons-material";
-import {FormControl} from "@mui/base";
-import React, {useState} from "react";
-import {User} from "@/app/lib/definitions";
+"use client";
+import { updateUserProfile } from "@/app/lib/actions";
+import {
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  TextField,
+} from "@mui/material";
+import {
+  AccountCircle,
+  Email,
+  Visibility,
+  VisibilityOff,
+} from "@mui/icons-material";
+import { FormControl } from "@mui/base";
+import React, { useState } from "react";
+import { User } from "@/app/lib/definitions";
 import Fab from "@mui/material/Fab";
 import DeleteIcon from "@mui/icons-material/Delete";
+import { useTranslation } from "react-i18next";
 
-export default function UsersTable({user}: { user: User }) {
-    const [isVisiblePass, setIsVisiblePass] = useState(false);
-
-    if (!user) return null;
-    const updateUserWithId = updateUserProfile.bind(null, user?.id);
-    const toggleVisblePass = () => {
-        setIsVisiblePass((prev) => !prev)
-    };
-    const handleMouseDownPassword = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-    };
-    return (
-
-        <Box
-            component="form" action={updateUserWithId} flexDirection={'column'}
-            sx={{alignItems: 'flex-start'}}
-            m={'1rem'}>
-            <Box component={'div'} display={'flex'} justifyContent={'center'}><Avatar
-                alt="Remy Sharp"
-                src={user?.imageUrl}
-                sx={{width: 56, height: 56, marginBottom: '1rem'}}
-            /></Box> <TextField
-            id="firstName"
-            name="firstName"
-            label="Edit first name"
-            defaultValue={user?.firstName}
-            variant="outlined"
-            fullWidth
-            sx={{mb: '1rem'}}
+export default function UsersTable({ user }: { user: User }) {
+  const [isVisiblePass, setIsVisiblePass] = useState(false);
+  const { t } = useTranslation();
+  if (!user) return null;
+  const updateUserWithId = updateUserProfile.bind(null, user?.id);
+  const toggleVisblePass = () => {
+    setIsVisiblePass((prev) => !prev);
+  };
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+  return (
+    <Box
+      component="form"
+      action={updateUserWithId}
+      flexDirection={"column"}
+      sx={{ alignItems: "flex-start" }}
+      m={"1rem"}
+    >
+      <Box component={"div"} display={"flex"} justifyContent={"center"}>
+        <Avatar
+          alt="Remy Sharp"
+          src={user?.imageUrl}
+          sx={{ width: 56, height: 56, marginBottom: "1rem" }}
         />
-            <Box sx={{borderRadius: '1%'}} p={1}>
-                <TextField
-                    sx={{marginBottom: '1rem'}}
-                    fullWidth={true}
-
-                    id="firstName"
-                    name="firstName"
-                    label="first name"
-                    defaultValue={user?.firstName}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <AccountCircle/>
-                            </InputAdornment>
-                        ),
-                    }}
-                    variant="standard"
-                    required={true}
-                />
-                <TextField
-                    sx={{marginBottom: '1rem'}}
-                    fullWidth={true}
-                    id="lastName"
-                    name="lastName"
-                    label="Last Name"
-                    defaultValue={user?.lastName}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <AccountCircle/>
-                            </InputAdornment>
-                        ),
-                    }}
-                    variant="standard"
-                    required={true}
-                />
-                <TextField
-                    sx={{marginBottom: '1rem'}}
-                    fullWidth={true}
-                    type={'email'}
-                    id="email"
-                    name="email"
-                    label="Email"
-                    defaultValue={user?.email}
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <Email/>
-                            </InputAdornment>
-                        ),
-                    }}
-                    variant="standard"
-                    required={true}
-                />
-                <FormControl>
-                    <InputLabel htmlFor="password">Password</InputLabel>
-                    <OutlinedInput
-                        id="password"
-                        name={'password'}
-                        type={isVisiblePass ? 'text' : 'password'}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={toggleVisblePass}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                >
-                                    {isVisiblePass ? <VisibilityOff/> : <Visibility/>}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                        label="Password"
-                        fullWidth={true}
-                        //onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
-                        required={true}
-                    />
-                </FormControl>
-                <FormControl>
-                    <InputLabel htmlFor="confirmPassword">Confirm Password</InputLabel>
-                    <OutlinedInput
-                        id="confirmPassword"
-                        name={'confirmPassword'}
-                        type={isVisiblePass ? 'text' : 'password'}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={toggleVisblePass}
-                                    onMouseDown={handleMouseDownPassword}
-                                    edge="end"
-                                >
-                                    {isVisiblePass ? <VisibilityOff/> : <Visibility/>}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                        label="Confirm password"
-                        fullWidth={true}
-                        //value={confirmPassword}
-                        //onChange={(e: React.ChangeEvent<HTMLInputElement>) => setConfirmPassword(e.target.value)}
-                        required={true}
-                    />
-                </FormControl>
-            </Box>
-            <Button endIcon={<DeleteIcon/>} color={'warning'} href={'/dashboard/profile-page/delete-account'}>
-                Delete your account
-            </Button>
-
-            <Box display={'flex'} justifyContent={'end'} mt={4}>
-                <Fab type="submit" variant={'extended'} color={'primary'}>Submit changes</Fab>
-
-            </Box>
-        </Box>
-    );
+      </Box>
+      {/*
+      reformat rest of file to use the new TextField component
+*/}
+      {/*<TextField
+        id="firstName"
+        name="firstName"
+        label={t("text_form_first_name")}
+        defaultValue={user?.firstName}
+        variant="outlined"
+        fullWidth
+        sx={{ mb: "1rem" }}
+      />*/}
+      <Box sx={{ borderRadius: "1%" }} p={1}>
+        <TextField
+          sx={{ marginBottom: "1rem" }}
+          fullWidth={true}
+          id="firstName"
+          name="firstName"
+          label={t("text_form_first_name")}
+          defaultValue={user?.firstName}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+          variant="standard"
+          required={true}
+        />
+        <TextField
+          sx={{ marginBottom: "1rem" }}
+          fullWidth={true}
+          id="lastName"
+          name="lastName"
+          label={t("text_form_last_name")}
+          defaultValue={user?.lastName}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <AccountCircle />
+              </InputAdornment>
+            ),
+          }}
+          variant="standard"
+          required={true}
+        />
+        <TextField
+          sx={{ marginBottom: "1rem" }}
+          fullWidth={true}
+          type={"email"}
+          id="email"
+          name="email"
+          label={t("text_form_email")}
+          defaultValue={user?.email}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <Email />
+              </InputAdornment>
+            ),
+          }}
+          variant="standard"
+          required={true}
+        />
+        <FormControl>
+          <InputLabel htmlFor="password">{t("text_form_password")}</InputLabel>
+          <OutlinedInput
+            id="password"
+            name={"password"}
+            type={isVisiblePass ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={toggleVisblePass}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {isVisiblePass ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+            fullWidth={true}
+            //onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+            required={true}
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="confirmPassword">
+            {t("text_form_confirm")}
+          </InputLabel>
+          <OutlinedInput
+            id="confirmPassword"
+            name={"confirmPassword"}
+            type={isVisiblePass ? "text" : "password"}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={toggleVisblePass}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {isVisiblePass ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Confirm password"
+            fullWidth={true}
+            //value={confirmPassword}
+            //onChange={(e: React.ChangeEvent<HTMLInputElement>) => setConfirmPassword(e.target.value)}
+            required={true}
+          />
+        </FormControl>
+      </Box>
+      <Button
+        endIcon={<DeleteIcon />}
+        color={"warning"}
+        href={"/dashboard/profile-page/delete-account"}
+      >
+        {t("text_button_delete")}{" "}
+      </Button>
+      <Box display={"flex"} justifyContent={"end"} mt={4}>
+        <Fab type="submit" variant={"extended"} color={"primary"}>
+          {t("text_button_submit")}{" "}
+        </Fab>
+      </Box>
+    </Box>
+  );
 }
