ee4dfaf2b671f5db9a98d9db0d827ed3a0b784fc
Guido Pierri <105157860+Guido-Pierri@users.noreply.github.com>
2024-09-14 13:21:32 +0200

    add translation to add item

--- pentry/app/dashboard/pantry/add-item/page.tsx
+++ pentry/app/dashboard/pantry/add-item/page.tsx
@@ -1,31 +1,15 @@
-import CreateForm from '@/app/ui/add-item/create-form'
-import {auth} from "@/auth";
-import {fetchCategories} from "@/app/lib/data";
-import {Box} from "@mui/material";
+import CreateForm from "@/app/ui/add-item/create-form";
+import { auth } from "@/auth";
+import { Box } from "@mui/material";
 
-export default async function Page({searchParams}: {
-    searchParams?: {
-        query?: string;
-        page?: string;
-    };
-}) {
-    const session = await auth();
-    const query = searchParams?.query ?? '';
-    const currentPage = Number(searchParams?.page) || 1;
-
-    const categories = await fetchCategories()
-    if (!session?.token) {
-        return null
-    }
-    if (!categories) {
-        return null
-    }
-    console.log('categories', categories)
-    return (
-        <Box flex={'auto'} flexDirection={'column'} alignContent={'center'}>
-            <CreateForm/>
-
-        </Box>
-    )
-
+export default async function Page() {
+  const session = await auth();
+  if (!session?.token) {
+    return null;
+  }
+  return (
+    <Box flex={"auto"} flexDirection={"column"} alignContent={"center"}>
+      <CreateForm />
+    </Box>
+  );
 }
